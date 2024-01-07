3470a4e55bea2b2cb978c60ffbcf8e110d06f8a5
Sharon <sharon.gomez@casebook.net>
2024-01-07 13:52:40 -0500

    fix auth form focus loss issue

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,6 +5,58 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button, Input, Stack, Text } from "@chakra-ui/react";
 import "../styles/auth.css";
 
+const SignUpFields = ({
+  setName,
+  setEmail,
+  setPassword,
+  name,
+  email,
+  password,
+}) => (
+  <>
+    <Input
+      type="text"
+      placeholder="name"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      variant="outline"
+    />
+    <Input
+      type="email"
+      placeholder="e-mail"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      variant="outline"
+    />
+    <Input
+      type="password"
+      placeholder="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      variant="outline"
+    />
+  </>
+);
+
+const LoginFields = ({ setEmail, setPassword, email, password }) => (
+  <>
+    <Input
+      type="email"
+      placeholder="e-mail"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      variant="outline"
+    />
+    <Input
+      type="password"
+      placeholder="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      variant="outline"
+    />
+  </>
+);
+
 const Auth = ({ isLoggingIn }: { isLoggingIn?: boolean }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -29,55 +81,6 @@ const Auth = ({ isLoggingIn }: { isLoggingIn?: boolean }) => {
     return;
   };
 
-  const SignUpFields = () => {
-    return (
-      <>
-        <Input
-          type="text"
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          variant="outline"
-        />
-        <Input
-          type="email"
-          placeholder="e-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          variant="outline"
-        />
-        <Input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          variant="outline"
-        />
-      </>
-    );
-  };
-
-  const LoginFields = () => {
-    return (
-      <>
-        <Input
-          type="email"
-          placeholder="e-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          variant="outline"
-        />
-        <Input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          variant="outline"
-        />
-      </>
-    );
-  };
-
   // todo - clean this up
   return (
     <div id="auth">
@@ -91,7 +94,23 @@ const Auth = ({ isLoggingIn }: { isLoggingIn?: boolean }) => {
       </Text>
       <form className="auth-form" onSubmit={handleSubmit}>
         <Stack className="auth-buttons" spacing={3}>
-          {isLoggingIn ? <LoginFields /> : <SignUpFields />}
+          {isLoggingIn ? (
+            <LoginFields
+              setEmail={setEmail}
+              setPassword={setPassword}
+              email={email}
+              password={password}
+            />
+          ) : (
+            <SignUpFields
+              setEmail={setEmail}
+              setPassword={setPassword}
+              setName={setName}
+              email={email}
+              password={password}
+              name={name}
+            />
+          )}
           <Button onClick={handleSubmit} type="submit" colorScheme="gray">
             {isLoggingIn ? "Log in" : "Sign up"}
           </Button>
